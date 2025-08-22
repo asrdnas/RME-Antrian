41dde427c8d6206de3e1387bcd11f72f8036bbf0
kopikap11 <rbaldi1222@gmail.com>
2025-08-22 09:32:43 +0700

    perbaikan di halaman create rekam medis (return edit selesai)

--- app/Filament/TenagaMedis/Resources/RekamMedisResource/Pages/CreateRekamMedis.php
+++ app/Filament/TenagaMedis/Resources/RekamMedisResource/Pages/CreateRekamMedis.php
@@ -9,4 +9,11 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateRekamMedis extends CreateRecord
 {
     protected static string $resource = RekamMedisResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        // Balik ke list Rekam Medis
+        return $this->getResource()::getUrl('index');
+    }
+    
 }
