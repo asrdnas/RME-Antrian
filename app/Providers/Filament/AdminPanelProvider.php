62ed2ae1fb1225585365584f770fe1ca2e6617cc
anas <radenanas12@gmail.com>
2026-04-08 21:32:47 +0700

    Change admin panel path to 'adminklinik'

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -25,7 +25,7 @@ class AdminPanelProvider extends PanelProvider
         return $panel
             ->default()
             ->id('admin')
-            ->path('admin')
+            ->path('adminklinik')
             ->authGuard('admin')
             ->login()
             ->favicon(asset('image.png'))
