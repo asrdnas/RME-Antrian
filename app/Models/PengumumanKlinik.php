a3a0c58c8ba1a81efd0053dfc0237a89a2957de4
anas <radenanas12@gmail.com>
2026-04-22 11:39:43 +0700

    Rename class PengumumanKlinik to pengumumanklinik

--- app/Models/PengumumanKlinik.php
+++ app/Models/PengumumanKlinik.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class PengumumanKlinik extends Model
+class pengumumanklinik extends Model
 {
     protected $fillable = [
         'navbar_id',
