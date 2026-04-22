2455901c1f04edce462fe77900550b7ff7865780
anas <radenanas12@gmail.com>
2026-04-22 11:44:45 +0700

    Fix class name casing for PengumumanKlinik

--- app/Models/PengumumanKlinik.php
+++ app/Models/PengumumanKlinik.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class pengumumanklinik extends Model
+class PengumumanKlinik extends Model
 {
     protected $fillable = [
         'navbar_id',
